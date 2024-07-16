0b5456871c71d3722bd63d399c25d44ecbeb084a
Boss <abdumuratovabduvali@07gmail.com>
2024-07-16 14:58:58 +0500

    fixed

--- oydin/translation.py
+++ oydin/translation.py
@@ -5,7 +5,8 @@ from .models import Product, Category
 
 @register(Product)
 class ProductTranslationOptions(TranslationOptions):
-    fields = ('name','description','extra_field_label','extra_field_value','dop_field_label','dop_field_value')
+    fields = ('name','description','extra_field_label','extra_field_value','dop_field_label','dop_field_value',\
+              'characteristic_n1','characteristic_n2','characteristic_n3')
 
 @register(Category)
 class CategoryTranslationOptions(TranslationOptions):
